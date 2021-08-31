684605c87031203268fa74c7a5eed8480a3f925d
Matt <matt@mattpolicastro.com>
2021-08-30 21:17:53 -0500

    Getting ready for deployment/hosting on GH pages

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Update the publicPath to reflect your hosting environment per the vue-cli deployment guide.
+  publicPath: process.env.NODE_ENV === 'production'
+    // ? '/mhm.social'
+    ? '/'
+    : '/'
+}
