32287257b1fd8f9b5b02230d14373aab981970e7
Matt <matt@mattpolicastro.com>
2022-05-05 10:52:59 -0500

    Updated footer to just point to my GH profile instead of the project repo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <h2>other stuff</h2>
     <p>hi! i'm matt and i do other stuff, too.</p>
     <ul>
-      <li><a href="https://github.com/mattpolicastro/mhm.social" target="_blank" rel="noopener">github</a></li>
+      <li><a href="https://github.com/mattpolicastro" target="_blank" rel="noopener">github</a></li>
       <li><a href="https://twitter.com/mattpolicastro" target="_blank" rel="noopener">twitter</a></li>
     </ul>
   </div>
