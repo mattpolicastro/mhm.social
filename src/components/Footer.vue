c55271773143cb210202ba7cb449f22aa2975d4e
Matt Policastro <matt@mattpolicastro.com>
2023-11-27 15:18:21 -0600

    Replace link to Twitter w/ Bluesky

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
     <p>hi! i'm matt and i do other stuff, too.</p>
     <ul>
       <li><a href="https://github.com/mattpolicastro" target="_blank" rel="noopener">github</a></li>
-      <li><a href="https://twitter.com/mattpolicastro" target="_blank" rel="noopener">twitter</a></li>
+      <li><a href="https://bsky.app/profile/mattpolicastro.bsky.social" target="_blank" rel="noopener">bluesky</a></li>
     </ul>
   </div>
 </template>
